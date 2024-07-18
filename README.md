33feb881ee2151352b9786c72f423c5068fb2e4a
Marian Beszedes <marian.beszedes@konicaminolta.cz>
2024-07-18 15:50:18 +0000

    Added functionality

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # My Python Project Template
 
+use `flit install`
+use `pre-commit check`
+
+Connfiguration for VsCode ruff and pre-commit ruff is in `pyproject.toml`
+
--- src/python_package/my_math.py
+++ src/python_package/my_math.py
@@ -1,6 +1,12 @@
 """This is a Sample Python file."""
 
 
-def add(a, b):
+def add(a: float, b: float) -> float:
     """Returns the sum of a and b."""
     return a + b
+
+
+def minus(a: float, b: float) -> float:
+    """Returns the difference of a and b."""
+
+    return a - b
